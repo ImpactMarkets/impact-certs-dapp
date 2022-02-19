dbb5a6e4180f5c8bc6a930973b65fdff6c10d575
Kenneth Bambridge <kbambridge1@gmail.com>
2022-02-19 13:10:43 -0700

    Made RatchetAuction an NFT

--- packages/hardhat/contracts/RatchetAuction.sol
+++ packages/hardhat/contracts/RatchetAuction.sol
@@ -7,78 +7,89 @@ pragma solidity >=0.7.0 <0.9.0;
 // We import this library to be able to use console.log
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol"; // TODO: use this to only accept nfts setup properly
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 
 // This is the main building block for smart contracts.
-contract RatchetAuction is IERC721Receiver {
+contract RatchetAuction is Context, IERC721, IERC721Metadata, IERC721Receiver, ERC165 {
+    using Address for address;
+    using Strings for uint256;
+
     // NFT -> Payout address
-    mapping(address => address) public issuer;
+    mapping(uint256 => address) public issuers;
     // The minimum percent increase in price to ratchet the auction
-    mapping(address => uint16) public minPercentRaise; // decimal 3, 1000 -> 1%
+    mapping(uint256 => uint16) public minPercentRaises; // decimal 3, 1000 -> 1%
     // The percent of the purchase that goes to the previous owner
-    mapping(address => uint16) public ownerFee; // decimal 3, 1000 -> 1%
+    mapping(uint256 => uint16) public ownerFees; // decimal 3, 1000 -> 1%
     // The erc20 that must be paid to purchase from this auction
-    mapping(address => IERC20) public auctionToken;
-    // The nft being sold
-    mapping(address => bool) public nftReceived;
+    mapping(uint256 => IERC20) public auctionTokens;
 
-    mapping(address => address) public owner; // The current owner of the NFT in the auction
-    mapping(address => address) public initiator; // The address that deposited the NFT and started the auction
+    mapping(uint256 => address) public initiators; // The address that deposited the NFT and started the auction
 
-    mapping(address => uint256) public lastPrice; // The last price paid by the buyer
+    mapping(uint256 => uint256) public lastPrices; // The last price paid by the buyer
 
+    uint256 private _count = 0;
+
+    // Token name
+    string private _name;
+
+    // Token symbol
+    string private _symbol;
+
+    // Mapping from token ID to owner address
+    mapping(uint256 => address) private _owners;
+
+    // Mapping owner address to token count
+    mapping(address => uint256) private _balances;
+
+    // Mapping from token ID to approved address
+    mapping(uint256 => address) private _tokenApprovals;
+
+    // Mapping from owner to operator approvals
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
     /**
-     * Contract initialization.
-     *
-     * The `constructor` is executed only once when the contract is created.
-     * The `public` modifier makes a function callable from outside the contract.
+     * @dev Initializes the contract by setting a `name` and a `symbol` to the token collection.
      */
-    constructor(address _issuer, uint16 _minPercentRaise, uint16 _ownerFee, address _auctionToken, uint256 _minBid ) {
-        // The totalSupply is assigned to transaction sender, which is the account
-        // that is deploying the contract.
-        issuer = _issuer;
-        minPercentRaise = _minPercentRaise;
-        ownerFee = _ownerFee;
-        auctionToken = IERC20(_auctionToken);
-        lastPrice = _minBid;
-        initiator = msg.sender;
-    }
-
-    /**
-     * Read only function to check if the owner is the sending account
-     *
-     * The `view` modifier indicates that it doesn't modify the contract's
-     * state, which allows us to call it without executing a transaction.
-     */
-    function isOwner(address account) external view returns (bool) {
-        return owner == account;
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
     }
 
     /**
      * Buyer calls this function to buy the NFT in the auction
      */
-    function buy(uint256 bid) external {
-        require(nftReceived, "The auction did not start yet");
+    function buy(uint256 bid, uint256 tokenId) external {
+        address owner = ownerOf(tokenId);
+        require(owner != address(0), "The auction did not start yet");
+        uint256 lastPrice = lastPrices[tokenId];
+        uint16 minPercentRaise = minPercentRaises[tokenId];
+        IERC20 auctionToken = auctionTokens[tokenId];
         uint256 minBid = lastPrice + lastPrice * minPercentRaise / 100000;
         require(bid >= minBid, "Bid must be higher than the last price + minimum raise");
-        require(auctionToken.allowance(msg.sender, address(this)) >= bid, "Buyer allowance too low");
+        require(auctionToken.allowance(_msgSender(), address(this)) >= bid, "Buyer allowance too low");
         uint256 amount = bid;
-        if (owner != address(0x0)) {
+        uint16 ownerFee = ownerFees[tokenId];
+        if (owner != address(0)) {
             uint256 ownerShare = 0;
             if (ownerFee > 0) {
                 ownerShare = (bid - lastPrice) * ownerFee / 100000; // TODO: Safe Math
             }
-            _safeTransferFrom(auctionToken, msg.sender, owner, lastPrice + ownerShare);
+            _safeERC20TransferFrom(auctionToken, _msgSender(), owner, lastPrice + ownerShare);
             amount = bid - lastPrice - ownerShare;
         }
-        _safeTransferFrom(auctionToken, msg.sender, issuer, amount);
-        owner = msg.sender;
-        lastPrice = bid;
+        address issuer = issuers[tokenId];
+        _safeERC20TransferFrom(auctionToken, _msgSender(), issuer, amount);
+        _transfer(owner, _msgSender(), tokenId);
+        lastPrices[tokenId] = bid;
     }
 
-    function _safeTransferFrom(
+    function _safeERC20TransferFrom(
         IERC20 token,
         address sender,
         address recipient,
@@ -88,20 +99,336 @@ contract RatchetAuction is IERC721Receiver {
         require(sent, "Token transfer failed");
     }
 
-    function start(address _nft, uint256 tokenId) public {
-        require(msg.sender == initiator, "Only initiator can start the auction");
+    function start(address _nft, uint256 tokenId, address issuer, uint16 minPercentRaise, uint16 ownerFee, address auctionToken, uint256 minBid) public {
         IERC721 nft = IERC721(_nft);
-        nft.safeTransferFrom(msg.sender, address(this), tokenId);
+        nft.transferFrom(_msgSender(), address(this), tokenId);
+        _count += 1;
+        _mint(_msgSender(), _count);
+        issuers[_count] = issuer;
+        minPercentRaises[_count] = minPercentRaise;
+        ownerFees[_count] = ownerFee;
+        auctionTokens[_count] = IERC20(auctionToken);
+        lastPrices[_count] = minBid;
+        initiators[_count] = _msgSender();
     }
 
     /**
-     * This gets called when a ERC721 is received by the contract
+     * @dev See {IERC165-supportsInterface}.
      */
-    function onERC721Received( address, address from, uint256, bytes calldata) external returns (bytes4) {
-        require(!nftReceived, "The auction must not have started");
-        require(from == initiator, "Only the initiator should send NFTs to this contract");
-        nftReceived = true;
-        console.log("onERC721Received");
-        return IERC721Receiver.onERC721Received.selector;
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165, IERC165) returns (bool) {
+        return
+            interfaceId == type(IERC721).interfaceId ||
+            interfaceId == type(IERC721Metadata).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+    /**
+     * @dev See {IERC721-balanceOf}.
+     */
+    function balanceOf(address owner) public view virtual override returns (uint256) {
+        require(owner != address(0), "ERC721: balance query for the zero address");
+        return _balances[owner];
+    }
+
+    /**
+     * @dev See {IERC721-ownerOf}.
+     */
+    function ownerOf(uint256 tokenId) public view virtual override returns (address) {
+        address owner = _owners[tokenId];
+        require(owner != address(0), "ERC721: owner query for nonexistent token");
+        return owner;
+    }
+
+    /**
+     * @dev See {IERC721Metadata-name}.
+     */
+    function name() public view virtual override returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev See {IERC721Metadata-symbol}.
+     */
+    function symbol() public view virtual override returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
+    }
+
+    /**
+     * @dev Base URI for computing {tokenURI}. If set, the resulting URI for each
+     * token will be the concatenation of the `baseURI` and the `tokenId`. Empty
+     * by default, can be overriden in child contracts.
+     */
+    function _baseURI() internal view virtual returns (string memory) {
+        return "";
+    }
+
+    /**
+     * @dev See {IERC721-approve}.
+     */
+    function approve(address to, uint256 tokenId) public virtual override {
+        address owner = ownerOf(tokenId);
+        require(to != owner, "ERC721: approval to current owner");
+
+        require(
+            _msgSender() == owner || isApprovedForAll(owner, _msgSender()),
+            "ERC721: approve caller is not owner nor approved for all"
+        );
+
+        _approve(to, tokenId);
+    }
+
+    /**
+     * @dev See {IERC721-getApproved}.
+     */
+    function getApproved(uint256 tokenId) public view virtual override returns (address) {
+        require(_exists(tokenId), "ERC721: approved query for nonexistent token");
+
+        return _tokenApprovals[tokenId];
+    }
+
+    /**
+     * @dev See {IERC721-setApprovalForAll}.
+     */
+    function setApprovalForAll(address operator, bool approved) public virtual override {
+        _setApprovalForAll(_msgSender(), operator, approved);
+    }
+
+    /**
+     * @dev See {IERC721-isApprovedForAll}.
+     */
+    function isApprovedForAll(address owner, address operator) public view virtual override returns (bool) {
+        return _operatorApprovals[owner][operator];
+    }
+    /**
+     * @dev Returns whether `tokenId` exists.
+     *
+     * Tokens can be managed by their owner or approved accounts via {approve} or {setApprovalForAll}.
+     *
+     * Tokens start existing when they are minted (`_mint`),
+     * and stop existing when they are burned (`_burn`).
+     */
+    function _exists(uint256 tokenId) internal view virtual returns (bool) {
+        return _owners[tokenId] != address(0);
+    }
+
+    /**
+     * @dev Mints `tokenId` and transfers it to `to`.
+     *
+     * WARNING: Usage of this method is discouraged, use {_safeMint} whenever possible
+     *
+     * Requirements:
+     *
+     * - `tokenId` must not exist.
+     * - `to` cannot be the zero address.
+     *
+     * Emits a {Transfer} event.
+     */
+    function _mint(address to, uint256 tokenId) internal virtual {
+        require(to != address(0), "ERC721: mint to the zero address");
+        require(!_exists(tokenId), "ERC721: token already minted");
+
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(address(0), to, tokenId);
+    }
+
+    /**
+     * @dev Safely mints `tokenId` and transfers it to `to`.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must not exist.
+     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
+     *
+     * Emits a {Transfer} event.
+     */
+    function _safeMint(address to, uint256 tokenId) internal virtual {
+        _safeMint(to, tokenId, "");
+    }
+
+    /**
+     * @dev Same as {xref-ERC721-_safeMint-address-uint256-}[`_safeMint`], with an additional `data` parameter which is
+     * forwarded in {IERC721Receiver-onERC721Received} to contract recipients.
+     */
+    function _safeMint(
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) internal virtual {
+        _mint(to, tokenId);
+        require(
+            _checkOnERC721Received(address(0), to, tokenId, _data),
+            "ERC721: transfer to non ERC721Receiver implementer"
+        );
+    }
+
+    /**
+     * @dev See {IERC721-transferFrom}.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual override {
+        //solhint-disable-next-line max-line-length
+        require(_msgSender() == address(this), "Only this contract can transfer these tokens");
+        _transfer(from, to, tokenId);
+    }
+
+    /**
+     * @dev Transfers `tokenId` from `from` to `to`.
+     *  As opposed to {transferFrom}, this imposes no restrictions on msg.sender.
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must be owned by `from`.
+     *
+     * Emits a {Transfer} event.
+     */
+    function _transfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual {
+        require(to != address(0), "ERC721: transfer to the zero address");
+
+        // Clear approvals from the previous owner
+        _approve(address(0), tokenId);
+
+        _balances[from] -= 1;
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(from, to, tokenId);
+    }
+
+    /**
+     * @dev Approve `to` to operate on `tokenId`
+     *
+     * Emits a {Approval} event.
+     */
+    function _approve(address to, uint256 tokenId) internal virtual {
+        _tokenApprovals[tokenId] = to;
+        emit Approval(ownerOf(tokenId), to, tokenId);
+    }
+
+    /**
+     * @dev Approve `operator` to operate on all of `owner` tokens
+     *
+     * Emits a {ApprovalForAll} event.
+     */
+    function _setApprovalForAll(
+        address owner,
+        address operator,
+        bool approved
+    ) internal virtual {
+        require(owner != operator, "ERC721: approve to caller");
+        _operatorApprovals[owner][operator] = approved;
+        emit ApprovalForAll(owner, operator, approved);
+    }
+
+
+    /**
+     * @dev See {IERC721-safeTransferFrom}.
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual override {
+        safeTransferFrom(from, to, tokenId, "");
+    }
+
+    /**
+     * @dev See {IERC721-safeTransferFrom}.
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) public virtual override {
+        require(_msgSender() == address(this), "Only this contract can transfer these tokens");
+        _safeTransfer(from, to, tokenId, _data);
+    }
+    /**
+     * @dev Safely transfers `tokenId` token from `from` to `to`, checking first that contract recipients
+     * are aware of the ERC721 protocol to prevent tokens from being forever locked.
+     *
+     * `_data` is additional data, it has no specified format and it is sent in call to `to`.
+     *
+     * This internal function is equivalent to {safeTransferFrom}, and can be used to e.g.
+     * implement alternative mechanisms to perform token transfer, such as signature-based.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must exist and be owned by `from`.
+     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
+     *
+     * Emits a {Transfer} event.
+     */
+    function _safeTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) internal virtual {
+        _transfer(from, to, tokenId);
+        require(_checkOnERC721Received(from, to, tokenId, _data), "ERC721: transfer to non ERC721Receiver implementer");
+    }
+
+    /**
+     * @dev Internal function to invoke {IERC721Receiver-onERC721Received} on a target address.
+     * The call is not executed if the target address is not a contract.
+     *
+     * @param from address representing the previous owner of the given token ID
+     * @param to target address that will receive the tokens
+     * @param tokenId uint256 ID of the token to be transferred
+     * @param _data bytes optional data to send along with the call
+     * @return bool whether the call correctly returned the expected magic value
+     */
+    function _checkOnERC721Received(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) private returns (bool) {
+        if (to.isContract()) {
+            try IERC721Receiver(to).onERC721Received(_msgSender(), from, tokenId, _data) returns (bytes4 retval) {
+                return retval == IERC721Receiver.onERC721Received.selector;
+            } catch (bytes memory reason) {
+                if (reason.length == 0) {
+                    revert("ERC721: transfer to non ERC721Receiver implementer");
+                } else {
+                    assembly {
+                        revert(add(32, reason), mload(reason))
+                    }
+                }
+            }
+        } else {
+            return true;
+        }
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) public returns (bytes4) {
+
     }
 }
--- packages/hardhat/test/index.ts
+++ packages/hardhat/test/index.ts
@@ -5,7 +5,6 @@
 // Hardhat tests are normally written with Mocha and Chai.
 
 // We import Chai to use its asserting functions here.
-import { parseEther } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 import { expect } from "chai";
 
@@ -53,13 +52,7 @@ describe("Ratchet Auction contract", function () {
     nft = await TestNFT.deploy();
     await nft.deployed();
 
-    auction = await RatchetAuction.deploy(
-      initiator.address,
-      50000,
-      1000,
-      token.address,
-      1000
-    );
+    auction = await RatchetAuction.deploy("Ratchet Auction", "RATCH");
     await auction.deployed();
 
     // Seed erc20 values in test addresses
@@ -69,50 +62,55 @@ describe("Ratchet Auction contract", function () {
 
   describe("Deployment", function () {
     it("Should set the right auction params", async function () {
-      expect(await auction.initiator()).to.equal(initiator.address);
-      expect(await auction.issuer()).to.equal(initiator.address);
-      expect(await auction.minPercentRaise()).to.equal(50000);
-      expect(await auction.ownerFee()).to.equal(1000);
-      expect(await auction.auctionToken()).to.equal(token.address);
-      expect(await auction.lastPrice()).to.equal(1000);
+      expect(await auction.name()).to.equal("Ratchet Auction");
+      expect(await auction.symbol()).to.equal("RATCH");
+      // expect(await auction.initiator()).to.equal(initiator.address);
+      // expect(await auction.issuer()).to.equal(initiator.address);
+      // expect(await auction.minPercentRaise()).to.equal(50000);
+      // expect(await auction.ownerFee()).to.equal(1000);
+      // expect(await auction.auctionToken()).to.equal(token.address);
+      // expect(await auction.lastPrice()).to.equal(1000);
     });
   });
 
   describe("Transactions", function () {
     it("Should fail when trying to buy before nft deposit", async function () {
-      await expect(auction.connect(addr1).buy(15000)).to.be.revertedWith(
-        "The auction did not start yet"
-      );
-    });
-    it("Should only allow one NFT send", async function () {
-      await nft.approve(auction.address, 1);
-      await expect(auction.start(nft.address, 1));
-      await nft.approve(auction.address, 2);
-      await expect(auction.start(nft.address, 2)).to.be.revertedWith(
-        "The auction must not have started"
+      await expect(auction.connect(addr1).buy(15000, 1)).to.be.revertedWith(
+        "ERC721: owner query for nonexistent token"
       );
     });
 
     it("Should respect ratchet auction rules", async function () {
       await nft.approve(auction.address, 1);
-      await auction.start(nft.address, 1);
+      await auction.start(
+        nft.address,
+        1,
+        initiator.address,
+        50000,
+        1000,
+        token.address,
+        1000
+      );
+      const tokenId = 1;
 
       const issuerBalance = await token.balanceOf(initiator.address);
       const addr1Balance = await token.balanceOf(addr1.address);
       await token.connect(addr1).approve(auction.address, 15000);
-      await auction.connect(addr1).buy(15000);
+      await auction.connect(addr1).buy(15000, tokenId);
       const addr1Balance2 = await token.balanceOf(addr1.address);
       const issuerBalance2 = await token.balanceOf(initiator.address);
       expect(issuerBalance2 - issuerBalance).to.equal(15000);
       expect(addr1Balance2 - addr1Balance).to.equal(-15000);
-      expect(await auction.owner()).to.equal(addr1.address);
+      expect(await auction.ownerOf(tokenId)).to.equal(addr1.address);
 
       const addr2Balance = await token.balanceOf(addr2.address);
       await token.connect(addr2).approve(auction.address, 30000);
-      await expect(auction.connect(addr2).buy(20000)).to.be.revertedWith(
+      await expect(
+        auction.connect(addr2).buy(20000, tokenId)
+      ).to.be.revertedWith(
         "Bid must be higher than the last price + minimum raise"
       );
-      await auction.connect(addr2).buy(30000);
+      await auction.connect(addr2).buy(30000, tokenId);
       const addr2Balance2 = await token.balanceOf(addr2.address);
       const addr1Balance3 = await token.balanceOf(addr1.address);
       const issuerBalance3 = await token.balanceOf(initiator.address);
@@ -123,9 +121,17 @@ describe("Ratchet Auction contract", function () {
 
     it("Should fail if buyer doesn’t have enough tokens", async function () {
       await nft.approve(auction.address, 1);
-      await auction.start(nft.address, 1);
+      await auction.start(
+        nft.address,
+        1,
+        initiator.address,
+        50000,
+        1000,
+        token.address,
+        1000
+      );
 
-      await expect(auction.connect(addr1).buy(150000)).to.be.revertedWith(
+      await expect(auction.connect(addr1).buy(150000, 1)).to.be.revertedWith(
         "Buyer allowance too low"
       );
     });
