9cf5cb94f7d82196b1757e39c03366d143cf5977
Kenneth Bambridge <kbambridge1@gmail.com>
2022-02-19 10:45:22 -0700

    Add pages

---
+++ packages/next/pages/cert/[address].tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const ImpactCertDetail: NextPage = () => {
+    return <div>Impace Cert details</div>
+}
+
+export default ImpactCertDetail;
---
+++ packages/next/pages/mint.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Minter: NextPage = () => {
+    return <div>Mint Page Here</div>
+}
+
+export default Minter;
