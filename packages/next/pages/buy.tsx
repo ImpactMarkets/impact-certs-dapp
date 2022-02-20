7e021b61806cd80379eed1eb8097a020c037e7f3
Kenneth Bambridge <kbambridge1@gmail.com>
2022-02-19 23:21:13 -0700

    Fix buy page price calculation

--- packages/next/pages/buy.tsx
+++ packages/next/pages/buy.tsx
@@ -5,7 +5,7 @@ import { useContract, useSigner, useNetwork, useWaitForTransaction, useContractR
 import erc20ABI from '../abis/erc20.json';
 import { Contract } from "ethers";
 import auctionABI from "../abis/ratchetauction.json";
-import { Button, TextInputField, Alert } from "evergreen-ui";
+import { Button, TextInputField, Alert, Spinner } from "evergreen-ui";
 import {
   Layout,
   WalletOptionsModal,
@@ -28,9 +28,10 @@ const BuyPage: NextPage = () => {
   const [txHash, setTxHash] = useState('');
 
   const checkPrice = async () => {
-        const lastPrice = await auction.lastPrices(tokenId);
+        const lastPriceBigNum = await auction.lastPrices(tokenId);
+        const lastPrice = lastPriceBigNum.toNumber();
         const minRaise = await auction.minPercentRaises(tokenId);
-        setBid(lastPrice * minRaise / 1000.0);
+        setBid(lastPrice + lastPrice * (minRaise / 100000.0));
     };
 
   const buyCert = async () => {
@@ -38,8 +39,10 @@ const BuyPage: NextPage = () => {
     const auctionToken = await auction.auctionTokens(tokenId);
     const erc20Contract = new Contract(auctionToken, erc20ABI, signer);
     const approvalTx = await erc20Contract.approve(auctionAddress, bid);
+    setTxHash(approvalTx.hash);
     const waitForApproval = await wait({ hash: approvalTx.hash });
     const bidTx = await auction.buy(bid, tokenId);
+    setTxHash(bidTx.hash);
     const waitForBid = await wait({ hash: bidTx.hash });
     setBuying(false);
   };
@@ -58,8 +61,9 @@ const BuyPage: NextPage = () => {
         <TextInputField label="ICA Token ID" value={tokenId} type="number" onChange={({ target }: any) => { setTokenId(target.value) }}/>
         <Button onClick={checkPrice}>Check Min Price</Button>
         <Button className="buy button" onClick={buyCert}>Buy</Button>
-          {txHash && (
+          {txHash &&
             <Alert intent="success" title="Your Auction is being created!">
+              <Spinner size={20} />
               View the transaction on{" "}
               <a
                 target="_blank"
@@ -67,8 +71,8 @@ const BuyPage: NextPage = () => {
                 rel="noreferrer"
               >
                 Etherscan
-              </a>{" "}
-            </Alert>)}
+              </a>
+            </Alert>}
         </div>
       </Layout>
     </Fragment>
