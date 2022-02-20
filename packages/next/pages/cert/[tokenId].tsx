be25caeb12a458b84488152e30a6be79cf32ec0b
Kenneth Bambridge <kbambridge1@gmail.com>
2022-02-19 18:45:22 -0700

    Fix cert tokenId detail page

--- packages/next/pages/cert/[tokenId].tsx
+++ packages/next/pages/cert/[tokenId].tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from "next";
+import { useRouter } from 'next/router'
 import { ReactNode } from "react";
 import type ImpactCert from "../../types/ImpactCert";
 
 interface Props {
     children: ReactNode;
-    cert: ImpactCert
 }
 
-const ImpactCertDetail: NextPage<Props> = ({ cert }: Props) => {
+const ImpactCertDetail: NextPage<Props> = () => {
+    const router = useRouter()
+    const { tokenId } = router.query
     return <div>Impace Cert details</div>
 }
 
